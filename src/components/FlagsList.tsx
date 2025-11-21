87b4d3e05a665fd85635bf5d6ccff028d7bc852f
Pex-Dev <brayanc.moya@gmail.com>
2025-11-21 11:04:31 -0300

    Add filteredCountryList to FlagsList component

--- src/components/FlagsList.tsx
+++ src/components/FlagsList.tsx
@@ -4,7 +4,7 @@ import { useCountry } from "../context/CountryContext";
 import Flag from "./Flag";
 
 export default function FlagList() {
-  const { countryList, loading } = useCountry();
+  const { countryList, filteredCountryList, loading } = useCountry();
   return (
     <div className="px-5 my-10 max-w-[1440px] mx-auto">
       <header className="flex flex-col gap-10 md:flex-row md:justify-between">
@@ -19,9 +19,13 @@ export default function FlagList() {
         </ul>
       ) : (
         <ul className="grid grid-cols-1 px-6 md:px-0 md:grid-cols-3 lg:grid-cols-4 gap-10 lg:gap-18 mt-10">
-          {countryList.map((country) => (
-            <Flag key={country.name.official} country={country} />
-          ))}
+          {filteredCountryList.length > 0
+            ? filteredCountryList.map((country) => (
+                <Flag key={country.name.official} country={country} />
+              ))
+            : countryList.map((country) => (
+                <Flag key={country.name.official} country={country} />
+              ))}
         </ul>
       )}
     </div>
