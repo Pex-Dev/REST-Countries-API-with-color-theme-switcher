f1224e8fb8fead12a1978d4bd2a842d47fd4c571
Pex-Dev <brayanc.moya@gmail.com>
2025-11-21 11:05:35 -0300

    Handle error state in App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import { useCountry } from "./context/CountryContext";
 import Header from "./components/Header";
 import FlagsList from "./components/FlagsList";
 import CountryDetails from "./components/CountryDetails";
+import Error from "./components/Error";
 
 function App() {
-  const { country } = useCountry();
+  const { country, error } = useCountry();
   return (
     <div className="min-h-dvh ">
       <Header />
-      {country ? <CountryDetails /> : <FlagsList />}
+      {error ? <Error /> : country ? <CountryDetails /> : <FlagsList />}
     </div>
   );
 }
